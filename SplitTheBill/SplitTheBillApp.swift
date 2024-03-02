5508f2ef2ecfc460b76dee1436bacc44f5f36e4f
paulshnurenko <paulsnurenko@gmail.com>
2024-03-02 19:10:50 +0100

    Initial Commit

---
+++ SplitTheBill/SplitTheBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitTheBillApp.swift
+//  SplitTheBill
+//
+//  Created by Pavlo Shnurenko on 02.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SplitTheBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
